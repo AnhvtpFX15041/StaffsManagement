eb57f7568067170c1c64fb9e2113530ee26bd193
Vu Thi Phuong Anh <anhvtpFX15041@funix.edu.vn>
2022-02-13 14:26:57 +0700

    Tách component Modal, thêm close button

--- src/components/StaffList.js
+++ src/components/StaffList.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from 'react';
 import { Card, CardText, CardImg, Button, Form, FormGroup, Label, Input, Col, 
-    ModalHeader, Modal, ModalBody, ModalFooter} from 'reactstrap';
+    ModalHeader, Modal, ModalBody} from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 
@@ -15,17 +15,103 @@ import { Link } from 'react-router-dom';
             </Card>  
         );
     }
+    function AddStaff(props) {
+        const [isOpen, setIsOpen] = useState(false);
+        const [state, setState] = useState({
+            id: props.staffs.length-1,
+            staffname: '',
+            doB: '',
+            salaryScale: '',
+            startDate: '',
+            department: '',
+            annualLeave: '',
+            overTime: '',
+            image: '/assets/images/alberto.png'
+        });
+        const handleInputChange = (e) => {
+            const target = e.target;
+            const name = target.name
+            setState({
+                
+                [name]: target.value
+            });
+        }
+        const themmoi = (e) => {
+            alert(state.id);
+        }
+        const toggleModal =() =>{
+            setIsOpen(!isOpen)
+        };
+        return(
+            <div>
+                <Button onClick={toggleModal} style = {{margin: 3, justifyContent: 'right'}}><i className="fa fa-plus"></i></Button>
+                <Modal isOpen={isOpen} onClose={toggleModal}>
+                    <ModalHeader>
+                        <h5>Thêm nhân viên</h5>
+                        <button onClick={toggleModal} class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={themmoi}>
+                            <FormGroup row>
+                                <Label htmlFor="name" lg={4} md={4} sm={12}>Tên</Label>
+                                <Col lg={4} md={6} sm={12}>
+                                    <Input lg={4} md={6} sm={12} type="text" id="name" name="staffname" value={state.staffname} onChange={handleInputChange}></Input>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="birth" lg={4} md={4} sm={12}>Ngày sinh</Label>
+                                <Col lg={4} md={6} sm={12}>
+                                    <Input type="date" id="birth" name="doB" value={state.doB} onChange={handleInputChange}></Input>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="enter" lg={4} md={4} sm={12}>Ngày vào công ty</Label>
+                                <Col lg={4} md={6} sm={12}>
+                                    <Input type="date" id="enter" value={state.startDate} onChange={handleInputChange} name="startDate"></Input>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="dept" lg={4} md={4} sm={12}>Phòng ban</Label>
+                                <Col lg={4} md={6} sm={12}>
+                                    <Input type="select" value={state.department}>
+                                        <option value="Dept01">Sale</option>
+                                        <option value="Dept02">HR</option>
+                                        <option value="Dept03">Marketing</option>
+                                        <option value="Dept04">IT</option>
+                                        <option value="Dept05">Finance</option>
+                                    </Input>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="scale" lg={4} md={4} sm={12}>Hệ số lương</Label>
+                                <Col lg={4} md={6} sm={12}>
+                                    <Input type="text" id="scale" placeholder="1.0 -> 3.0" name="salaryScale" value={state.salaryScale} onChange={handleInputChange}></Input>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="leave" lg={4} md={4} sm={12}>Số ngày nghỉ còn lại</Label>
+                                <Col lg={4} md={6} sm={12}>
+                                    <Input type="text" id="leave" placeholder="1.0" name="annualLeave" value={state.annualLeave} onChange={handleInputChange}></Input>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="overtime" lg={4} md={4} sm={12}>Số ngày đã làm thêm</Label>
+                                <Col lg={4} md={6} sm={12}>
+                                    <Input type="text" id="overtime" placeholder="1.0" name="overTime" value={state.overTime} onChange={handleInputChange}></Input>
+                                </Col>
+                            </FormGroup>
+                            <Input type="submit" value="Thêm"/>
+                        </Form>
+                    </ModalBody>
+                </Modal>
+            </div>
+        )
+    }
     function List (props) { 
         const [staffs, setStaffs] = useState(props.staffs);
         const searchText = useRef(null);
-        const [isOpen, setIsOpen] = useState(false);
-        const showModal = () => {
-            setIsOpen(true);
-          };
-        
-          const hideModal = () => {
-            setIsOpen(false);
-          };
         const search =(e)=>{
             e.preventDefault();
             const searchval = searchText.current.value.toLowerCase();
@@ -45,7 +131,7 @@ import { Link } from 'react-router-dom';
                         <div className="row">
                             <div style={{display: 'flex'}} className="col-12 col-md-4 m-0">
                                 <h3 style ={{marginTop: 2}}>Nhân viên</h3> 
-                                <Button onClick={showModal} style = {{margin: 3, justifyContent: 'right'}}><i className="fa fa-plus"></i></Button>
+                                <AddStaff staffs={props.staffs}/>
                             </div>
                             <Form onSubmit = {search} className="col-12 col-md-8 m-0" style={{display: 'flex', justifyContent: 'right'}}>
                                 <Input style = {{margin: 3}}type="text" placeholder="Nguyễn Văn A" innerRef={searchText}/>
@@ -58,64 +144,6 @@ import { Link } from 'react-router-dom';
                 <div className="row">
                     {list}
                 </div>
-                <Modal isOpen={isOpen} onHide={hideModal}>
-                    <ModalHeader>Thêm nhân viên</ModalHeader>
-                    <ModalBody>
-                        <Form>
-                            <FormGroup row>
-                                <Label htmlFor="name" lg={4} md={4} sm={12}>Tên</Label>
-                                <Col lg={4} md={6} sm={12}>
-                                    <Input lg={4} md={6} sm={12} type="text" id="name" name="name"></Input>
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row>
-                                <Label htmlFor="birth" lg={4} md={4} sm={12}>Ngày sinh</Label>
-                                <Col lg={4} md={6} sm={12}>
-                                    <Input type="date" id="birth" name="birth"></Input>
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row>
-                                <Label htmlFor="enter" lg={4} md={4} sm={12}>Ngày vào công ty</Label>
-                                <Col lg={4} md={6} sm={12}>
-                                    <Input type="date" id="enter" name="enter"></Input>
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row>
-                                <Label htmlFor="dept" lg={4} md={4} sm={12}>Phòng ban</Label>
-                                <Col lg={4} md={6} sm={12}>
-                                    <Input type="select">
-                                        <option>Sale</option>
-                                        <option>HR</option>
-                                        <option>Marketing</option>
-                                        <option>IT</option>
-                                        <option>Finance</option>
-                                    </Input>
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row>
-                                <Label htmlFor="scale" lg={4} md={4} sm={12}>Hệ số lương</Label>
-                                <Col lg={4} md={6} sm={12}>
-                                    <Input type="text" id="scale" placeholder="1.0 -> 3.0" name="scale"></Input>
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row>
-                                <Label htmlFor="leave" lg={4} md={4} sm={12}>Số ngày nghỉ còn lại</Label>
-                                <Col lg={4} md={6} sm={12}>
-                                    <Input type="text" id="leave" placeholder="1.0" name="leave"></Input>
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row>
-                                <Label htmlFor="overtime" lg={4} md={4} sm={12}>Số ngày đã làm thêm</Label>
-                                <Col lg={4} md={6} sm={12}>
-                                    <Input type="text" id="overtime" placeholder="1.0" name="overtime"></Input>
-                                </Col>
-                            </FormGroup>
-                        </Form>
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button>Thêm</Button>
-                    </ModalFooter>
-                </Modal>
             </div>
         )
     }
